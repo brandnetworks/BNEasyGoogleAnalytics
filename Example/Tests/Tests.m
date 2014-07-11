87e7c2e654e03bac4efef12dffe44340ce066bd4
Ben Nicholas <bn@bn.co>
2014-07-11 15:37:20 -0400

    Start testing things - just the setup and shared instance so far

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,23 +6,71 @@
 //  Copyright (c) 2014 Ben Nicholas. All rights reserved.
 //
 
-SpecBegin(InitialSpecs)
+#import "objc/runtime.h"
+#import <BNEasyGoogleAnalytics/BNEasyGoogleAnalyticsTracker.h>
+#import <GoogleAnalytics-iOS-SDK/GAI.h>
 
-describe(@"these will pass", ^{
+void SwizzleClassMethod(Class c, SEL orig, SEL new) {
     
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
-    });
+    Method origMethod = class_getClassMethod(c, orig);
+    Method newMethod = class_getClassMethod(c, new);
     
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
-    });
+    c = object_getClass((id)c);
     
-    it(@"will wait and fail", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
+    if(class_addMethod(c, orig, method_getImplementation(newMethod), method_getTypeEncoding(newMethod)))
+        class_replaceMethod(c, new, method_getImplementation(origMethod), method_getTypeEncoding(origMethod));
+    else
+        method_exchangeImplementations(origMethod, newMethod);
+}
+
+GAI *sharedGAIMock;
+
+@interface GAI (SharedInstanceTestSwizzle)
+
++(GAI *)xxx_sharedInstance;
+
+@end
+
+@implementation GAI (SharedInstanceTestSwizzle)
+
++ (GAI *)xxx_sharedInstance
+{
+    return sharedGAIMock;
+}
+
+@end
+
+SpecBegin(TrackerSpec)
+
+describe(@"Easy Google Analytics Tracker", ^{
+    
+    context(@"Setup", ^{
+        it(@"should call into the google analytics setup method", ^{
+            // setup
+            sharedGAIMock = mock([GAI class]);
+            SwizzleClassMethod([GAI class], @selector(sharedInstance), @selector(xxx_sharedInstance));
+            
+            // exercise
+            [BNEasyGoogleAnalyticsTracker startWithTrackingId:@"tracking-hello"];
+            
+            // verify
+            [verify(sharedGAIMock) trackerWithTrackingId:@"tracking-hello"];
+            
+            // teardown
+            SwizzleClassMethod([GAI class], @selector(sharedInstance), @selector(xxx_sharedInstance));
         });
     });
+    
+    context(@"Shared Instance", ^{
+        it(@"Should return an instance", ^{
+            expect([BNEasyGoogleAnalyticsTracker sharedTracker]).toNot.beNil();
+        });
+        
+        it(@"Should return the same instance across calls", ^{
+            expect([BNEasyGoogleAnalyticsTracker sharedTracker]).to.beIdenticalTo([BNEasyGoogleAnalyticsTracker sharedTracker]);
+        });
+    });
+    
 });
 
 SpecEnd
