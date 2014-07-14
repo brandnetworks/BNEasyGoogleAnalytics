051ccd0918532450d4a0f88ce4cb6f91faf7947b
Ben Nicholas <bn@bn.co>
2014-07-14 13:58:17 -0400

    Add docs for internal methods

--- Pod/Classes/BNEasyGoogleAnalyticsTracker_private.h
+++ Pod/Classes/BNEasyGoogleAnalyticsTracker_private.h
@@ -10,8 +10,18 @@
 
 @interface BNEasyGoogleAnalyticsTracker ()
 
+/**
+ *  The internal GAITracker being used by the system
+ */
 @property (nonatomic, strong) id<GAITracker> tracker;
 
+/**
+ *  Internal only initializer for creating a Tracker given a GAITracker
+ *
+ *  @param tracker A GAITracker being provided by the underlying Google Analytics SDK
+ *
+ *  @return An initialized easy tracker.
+ */
 - (instancetype)initWithTracker:(id)tracker;
 
 @end
